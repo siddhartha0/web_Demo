bd92ff001ffc590c7481beb008d55cb5d259e836
siddhartha0 <siddhartha.sunuwar@gmail.com>
2022-12-02 15:35:41 +0545

    First

---
+++ src/main/java/com/mycompany/web/WebLoad.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.web;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServlet;
+
+
+public class WebLoad extends HttpServlet{
+    
+    
+}
